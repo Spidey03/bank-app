65c36003a62ae3df19b02fa098064acf2cc1be4d
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-08-15 11:08:42 +0530

    initialize components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import styles from './style';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
+    <div className="bg-primary w-full overflow-hidden App">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          NAVBAR
+        </div>
+      </div>
+  
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          HERO
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          STATS
+          BUSINESS
+          BILLING
+          CARD DEAL
+          TESTIMONIAL
+          CLIENTS
+          CTA
+          FOOTER
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/GeetStarted.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GeetStarted = () => {
+  return (
+    <div>GeetStarted</div>
+  )
+}
+
+export default GeetStarted
